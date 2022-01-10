0619406bfc323cf875f0652155152e195fe7c1b8
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2022-01-10 23:15:12 +0300

    Lesson 1

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Vector2Int _boardSize;
+    [SerializeField] private GameBoard _board;
+
+    private void Start()
+    {
+        _board.Initialize(_boardSize);
+    }
+}
